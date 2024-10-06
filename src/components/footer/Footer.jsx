7aa31f7ab5ea896e72b827567baabcabb0288c5a
Akbarjon <04ilhamova06@gmail.com>
2024-10-06 18:16:49 +0500

    loyiha yaratildi

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <p>All Right Reserved. <a href="https://www.youtube.com/@akhrorweb">Akhror Soliyev</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
